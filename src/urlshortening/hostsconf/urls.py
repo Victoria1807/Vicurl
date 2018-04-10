403ae8881b826d6e379739820f9b9f462c4c4204
Victoria1807 <flamaster1807@gmail.com>
2018-04-10 15:20:49 +0300

    Update allowed hosts

--- src/urlshortening/hostsconf/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from .views import wildcart_redirect
-
-urlpatterns = [
-    url(r'^(?P<path>.*)', wildcart_redirect),
-]
--- urlshortening/settings/production.py
+++ urlshortening/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x!g==abcapa!l&8@li0&y4d6+^-6lz*d1!z3i6$7(v-d&x10(p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.vicurl.com', 'vicurl.com']
+ALLOWED_HOSTS = ['www.vicurl.com', 'vicurl.com', 'vicurl.herokuapp.com']
 
 
 # Application definition
