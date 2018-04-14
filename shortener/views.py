0e6ae938d0da6a1b153164acd51d9bf1d1cca700
Victoria1807 <flamaster1807@gmail.com>
2018-04-14 15:56:13 +0300

    Updated to basic structure.

--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views import View
 
 from analytics.models import ClickEvent
